68745d114111474578724884f2a2167836f49bec
AnilcPaled <111037525+AnilcPaled@users.noreply.github.com>
2022-08-11 11:17:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("HelloWorld Come to Karnataka")
+    System.out.println("HelloWorld Come to Karnataka");
   }
 }
